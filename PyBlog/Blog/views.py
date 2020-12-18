178146100b2d5a9e2f417d0badfa007e01396a10
RocTanweer <mohammadtanweer769@gmail.com>
2020-12-18 09:41:41 +0530

    Views is changed from dummy data to post model data

--- PyBlog/Blog/views.py
+++ PyBlog/Blog/views.py
@@ -1,5 +1,7 @@
 from django.http import request
 from django.shortcuts import render 
+from .models import Post
+from django.contrib.auth.models import User
 # from django.http import HttpResponse
 # I am going to use render rather than HttpResponse now!
 
@@ -13,35 +15,35 @@ from django.shortcuts import render
 
 #Some Dummy Data for now!
 
-posts = [
-    {
-        'author' : 'roc_tanweer',
-        'date_posted' : '19-12-2020',
-        'title' : 'Blog-1',
-        'content' : 'This is the first blog'
-    },
+# posts = [
+#     {
+#         'author' : 'roc_tanweer',
+#         'date_posted' : '19-12-2020',
+#         'title' : 'Blog-1',
+#         'content' : 'This is the first blog'
+#     },
 
-    {
-         'author' : 'roc_tanweer',
-        'date_posted' : '20-12-2020',
-        'title' : 'Blog-2',
-        'content' : 'This is the second blog'
-    },
+#     {
+#          'author' : 'roc_tanweer',
+#         'date_posted' : '20-12-2020',
+#         'title' : 'Blog-2',
+#         'content' : 'This is the second blog'
+#     },
 
-    {
-         'author' : 'roc_tanweer',
-        'date_posted' : '21-12-2020',
-        'title' : 'Blog-3',
-        'content' : 'This is the third blog'
-    }
-]
+#     {
+#          'author' : 'roc_tanweer',
+#         'date_posted' : '21-12-2020',
+#         'title' : 'Blog-3',
+#         'content' : 'This is the third blog'
+#     }
+# ]
 
 
 
 
 def home(requests):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(requests, 'Blog/home.html', context)
 
