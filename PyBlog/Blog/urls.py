da6641576ea63d0d30f447c0c6e26191e4c1a6fb
RocTanweer <mohammadtanweer769@gmail.com>
2020-12-22 11:43:21 +0530

    added urls patterns for class-based views with <int:pk>

--- PyBlog/Blog/urls.py
+++ PyBlog/Blog/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     #using class based view for blog-home
     path('', views.PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post-update'),
     path('about/', views.about, name='blog-about'),
 ]
